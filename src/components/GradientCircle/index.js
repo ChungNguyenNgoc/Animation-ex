7c2875b6ff6ef47b0f838116412b1ecf91102eed
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2022-07-11 11:05:24 +0700

    update code

--- src/components/GradientCircle/index.js
+++ src/components/GradientCircle/index.js
@@ -6,18 +6,42 @@ const GradientCircle = () => {
     <div className="wrap-circle">
       <div className="circle">
         <ul className="cir-ul">
-          <li className="cir-li-1"><span className="cir-span-1">1</span></li>
-          <li className="cir-li-2"><span className="cir-span-2">2</span></li>
-          <li className="cir-li-3"><span className="cir-span-3">3</span></li>
-          <li className="cir-li-4"><span className="cir-span-4">4</span></li>
-          <li className="cir-li-5"><span className="cir-span-5">5</span></li>
-          <li className="cir-li-6"><span className="cir-span-6">6</span></li>
-          <li className="cir-li-7"><span className="cir-span-7">7</span></li>
-          <li className="cir-li-8"><span className="cir-span-8">8</span></li>
-          <li className="cir-li-9"><span className="cir-span-9">9</span></li>
-          <li className="cir-li-10"><span className="cir-span-10">10</span></li>
-          <li className="cir-li-11"><span className="cir-span-11">11</span></li>
-          <li className="cir-li-12"><span className="cir-span-12">12</span></li>
+          <li style={{ "--chung": 1 }} className="cir-li">
+            <span className="cir-span">1</span>
+          </li>
+          <li style={{ "--chung": 2 }} className="cir-li">
+            <span className="cir-span">2</span>
+          </li>
+          <li style={{ "--chung": 3 }} className="cir-li">
+            <span className="cir-span">3</span>
+          </li>
+          <li style={{ "--chung": 4 }} className="cir-li">
+            <span className="cir-span">4</span>
+          </li>
+          <li style={{ "--chung": 5 }} className="cir-li">
+            <span className="cir-span">5</span>
+          </li>
+          <li style={{ "--chung": 6 }} className="cir-li">
+            <span className="cir-span">6</span>
+          </li>
+          <li style={{ "--chung": 7 }} className="cir-li">
+            <span className="cir-span">7</span>
+          </li>
+          <li style={{ "--chung": 8 }} className="cir-li">
+            <span className="cir-span">8</span>
+          </li>
+          <li style={{ "--chung": 9 }} className="cir-li">
+            <span className="cir-span">9</span>
+          </li>
+          <li style={{ "--chung": 10 }} className="cir-li">
+            <span className="cir-span">10</span>
+          </li>
+          <li style={{ "--chung": 11 }} className="cir-li">
+            <span className="cir-span">11</span>
+          </li>
+          <li style={{ "--chung": 12 }} className="cir-li">
+            <span className="cir-span">12</span>
+          </li>
         </ul>
       </div>
     </div>
