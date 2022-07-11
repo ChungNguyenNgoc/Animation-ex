e43364aa7d299ee881757f6804ba7b54e7c4c1b3
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2022-07-11 08:24:14 +0700

    first commit

---
+++ src/components/GradientCircle/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.scss";
+
+const GradientCircle = () => {
+  return (
+    <div className="wrap-circle">
+      <div className="circle">
+        <ul className="cir-ul">
+          <li className="cir-li-1"><span className="cir-span-1">1</span></li>
+          <li className="cir-li-2"><span className="cir-span-2">2</span></li>
+          <li className="cir-li-3"><span className="cir-span-3">3</span></li>
+          <li className="cir-li-4"><span className="cir-span-4">4</span></li>
+          <li className="cir-li-5"><span className="cir-span-5">5</span></li>
+          <li className="cir-li-6"><span className="cir-span-6">6</span></li>
+          <li className="cir-li-7"><span className="cir-span-7">7</span></li>
+          <li className="cir-li-8"><span className="cir-span-8">8</span></li>
+          <li className="cir-li-9"><span className="cir-span-9">9</span></li>
+          <li className="cir-li-10"><span className="cir-span-10">10</span></li>
+          <li className="cir-li-11"><span className="cir-span-11">11</span></li>
+          <li className="cir-li-12"><span className="cir-span-12">12</span></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default GradientCircle;
