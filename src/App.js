5a41d59343734b7233379668ae218f0507fe3f1d
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2023-02-07 16:32:11 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import GradientCircle from "./components/GradientCircle";
 import ScrollWebpage from "./components/ScrollWebpage";
 import ToastMessage from "./components/ToastMessage";
 import VideoBackground from "./components/VideoBackground";
+import HoverEffects from "./components/HoverEffects";
 import "./styles.scss";
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
       {/* <ScrollWebpage /> */}
       {/* <VideoBackground /> */}
       {/* <CardEffects /> */}
-      <ToastMessage />
+      {/* <ToastMessage /> */}
+      <HoverEffects />
     </div>
   );
 }
---
+++ src/components/HoverEffects/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.scss";
+
+const HoverEffects = () => {
+  return (
+    <div className="hover-effects">
+      <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default HoverEffects;
