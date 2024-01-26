548db9f334906f96637624299f47976d6453a5cd
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2024-01-26 10:20:23 +0700

    update code

--- src/components/DrawCanvas/index.js
+++ src/components/DrawCanvas/index.js
@@ -9,6 +9,7 @@ const DrawCanvas = () => {
     width: null,
     height: null,
   });
+  const [selectedPointIndex, setSelectedPointIndex] = useState(null);
 
   const randomColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -19,62 +20,93 @@ const DrawCanvas = () => {
     setClientSize({ width: clientWidth, height: clientHeight });
   };
 
-  const handleCanvasClick = (e, setCoordinates) => {
-    const canvasTarget = e.target;
-    const rect = canvasTarget.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    setCoordinates((prev) => {
-      if (prev?.some((it) => it.x === x && it.y === y)) {
-        return [...prev];
-      }
-      return [...prev, { x, y }];
-    });
-  };
-
-  const drawOnCanvas = (coordinates, canvas) => {
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw a circle at each point
-    coordinates.forEach((it) => {
-      ctx.beginPath();
-      ctx.arc(it.x, it.y, 5, 0, 2 * Math.PI);
-      ctx.fillStyle = randomColor();
-      ctx.fill();
-    });
-
-    // Draw lines connecting the points
-    ctx.strokeStyle = randomColor();
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    coordinates.forEach((it, index) => {
-      if (index === 0) {
-        ctx.moveTo(it.x, it.y);
-      } else {
-        ctx.lineTo(it.x, it.y);
-      }
-    });
-    ctx.stroke();
-    ctx.fill();
-  };
-
   useEffect(() => {
     const canvas = document.querySelector("canvas");
-    drawOnCanvas(coordinates, canvas);
+
+    const handleCanvasClick = (e) => {
+      const canvasTarget = e.target;
+      const rect = canvasTarget.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      const clickedPointIndex = coordinates.findIndex((point) => {
+        const distance = Math.sqrt(
+          Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2),
+        );
+        return distance <= 6;
+      });
+
+      if (clickedPointIndex !== -1) {
+        setSelectedPointIndex(clickedPointIndex);
+      } else {
+        setCoordinates((prev) => {
+          if (prev?.some((it) => it.x === x && it.y === y)) {
+            return [...prev];
+          }
+          return [...prev, { x, y }];
+        });
+      }
+    };
+
+    const handleMouseMove = (e) => {
+      if (selectedPointIndex !== null) {
+        const canvasTarget = e.target;
+        const rect = canvasTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        setCoordinates((prevPoints) =>
+          prevPoints.map((point, index) =>
+            index === selectedPointIndex ? { x, y } : point,
+          ),
+        );
+      }
+    };
+
+    const handleMouseUp = () => {
+      setSelectedPointIndex(null);
+    };
+
+    const drawOnCanvas = () => {
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Draw a circle at each point
+      coordinates.forEach((it) => {
+        ctx.beginPath();
+        ctx.arc(it.x, it.y, 6, 0, 2 * Math.PI);
+        ctx.fillStyle = randomColor();
+        ctx.fill();
+      });
+
+      // Draw lines connecting the points
+      ctx.strokeStyle = randomColor();
+      ctx.lineWidth = 2;
+      ctx.beginPath();
+      coordinates.forEach((it, index) => {
+        if (index === 0) {
+          ctx.moveTo(it.x, it.y);
+        } else {
+          ctx.lineTo(it.x, it.y);
+        }
+      });
+      ctx.stroke();
+      ctx.fill();
+    };
+
+    drawOnCanvas();
 
     if (canvas) {
-      const getCursorPosition = (e) => {
-        handleCanvasClick(e, setCoordinates);
-      };
-
-      canvas.addEventListener("click", getCursorPosition);
+      canvas.addEventListener("click", handleCanvasClick);
+      canvas.addEventListener("mousemove", handleMouseMove);
+      canvas.addEventListener("mouseup", handleMouseUp);
       return () => {
-        canvas.removeEventListener("click", getCursorPosition);
+        canvas.removeEventListener("click", handleCanvasClick);
+        canvas.removeEventListener("mousemove", handleMouseMove);
+        canvas.removeEventListener("mouseup", handleMouseUp);
       };
     }
-  }, [coordinates]);
+  }, [coordinates, selectedPointIndex]);
 
   return (
     <div className="draw-canvas">
