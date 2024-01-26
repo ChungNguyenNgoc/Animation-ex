e1cd3d0828678d4cc513eba87b43e85081ba248d
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2024-01-26 09:27:45 +0700

    update code

--- src/components/DrawCanvas/index.js
+++ src/components/DrawCanvas/index.js
@@ -14,46 +14,31 @@ const DrawCanvas = () => {
     height: null,
   });
 
-  const randomColor = () => {
-    return "#" + Math.floor(Math.random() * 16777215).toString(16);
-  };
-
   const handleImageLoad = (e) => {
     const { naturalWidth, naturalHeight } = e.target;
     setNaturalSize({ width: naturalWidth, height: naturalHeight });
   };
 
-  useEffect(() => {
-    const img = document.getElementById("imageId123");
-    setClientSize({ width: img.clientWidth, height: img.clientHeight });
+  const randomColor = () => {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  };
 
-    if (img) {
-      const getCursorPosition = (e) => {
-        const rect = img.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
+  const handleImageClick = (e, coordinates, setCoordinates) => {
+    const img = e.target;
+    const rect = img.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
 
-        setCoordinates((prev) => {
-          if (prev?.some((it) => it.x === x && it.y === y)) {
-            return [...prev];
-          }
-          return [...prev, { x, y }];
-        });
-      };
+    setCoordinates((prev) => {
+      if (prev?.some((it) => it.x === x && it.y === y)) {
+        return [...prev];
+      }
+      return [...prev, { x, y }];
+    });
+  };
 
-      img.addEventListener("click", (e) => {
-        getCursorPosition(e);
-      });
-      return () => {
-        img.removeEventListener("click", getCursorPosition);
-      };
-    }
-  }, []);
-
-  useEffect(() => {
-    const canvas = document.querySelector("canvas");
+  const drawOnCanvas = (coordinates, canvas) => {
     const ctx = canvas.getContext("2d");
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw a circle at each point
@@ -77,6 +62,27 @@ const DrawCanvas = () => {
     });
     ctx.stroke();
     ctx.fill();
+  };
+
+  useEffect(() => {
+    const img = document.getElementById("imageId123");
+    setClientSize({ width: img.clientWidth, height: img.clientHeight });
+
+    if (img) {
+      const getCursorPosition = (e) => {
+        handleImageClick(e, coordinates, setCoordinates);
+      };
+
+      img.addEventListener("click", getCursorPosition);
+      return () => {
+        img.removeEventListener("click", getCursorPosition);
+      };
+    }
+  }, [coordinates]);
+
+  useEffect(() => {
+    const canvas = document.querySelector("canvas");
+    drawOnCanvas(coordinates, canvas);
   }, [coordinates]);
 
   return (
