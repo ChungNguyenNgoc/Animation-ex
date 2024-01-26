f6e9868ad6c5a06101ecfa0c0432a3d998a4c018
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2024-01-26 10:08:00 +0700

    update code

---
+++ src/components/DrawCanvas/index.js
@@ -0,0 +1,94 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import "./style.scss";
+import QuocHocHue from "./quoc_hoc_hue.jpg";
+
+const DrawCanvas = () => {
+  const [coordinates, setCoordinates] = useState([]);
+  const [clientSize, setClientSize] = useState({
+    width: null,
+    height: null,
+  });
+
+  const randomColor = () => {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
+  const handleImageLoad = (e) => {
+    const { clientWidth, clientHeight } = e.target;
+    setClientSize({ width: clientWidth, height: clientHeight });
+  };
+
+  const handleCanvasClick = (e, setCoordinates) => {
+    const canvasTarget = e.target;
+    const rect = canvasTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    setCoordinates((prev) => {
+      if (prev?.some((it) => it.x === x && it.y === y)) {
+        return [...prev];
+      }
+      return [...prev, { x, y }];
+    });
+  };
+
+  const drawOnCanvas = (coordinates, canvas) => {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw a circle at each point
+    coordinates.forEach((it) => {
+      ctx.beginPath();
+      ctx.arc(it.x, it.y, 5, 0, 2 * Math.PI);
+      ctx.fillStyle = randomColor();
+      ctx.fill();
+    });
+
+    // Draw lines connecting the points
+    ctx.strokeStyle = randomColor();
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    coordinates.forEach((it, index) => {
+      if (index === 0) {
+        ctx.moveTo(it.x, it.y);
+      } else {
+        ctx.lineTo(it.x, it.y);
+      }
+    });
+    ctx.stroke();
+    ctx.fill();
+  };
+
+  useEffect(() => {
+    const canvas = document.querySelector("canvas");
+    drawOnCanvas(coordinates, canvas);
+
+    if (canvas) {
+      const getCursorPosition = (e) => {
+        handleCanvasClick(e, setCoordinates);
+      };
+
+      canvas.addEventListener("click", getCursorPosition);
+      return () => {
+        canvas.removeEventListener("click", getCursorPosition);
+      };
+    }
+  }, [coordinates]);
+
+  return (
+    <div className="draw-canvas">
+      <div className="draw-canvas_wrap-img">
+        <img src={QuocHocHue} alt="QuocHocHue" onLoad={handleImageLoad} />
+        <canvas
+          id="canvasId123"
+          className="draw-canvas_wrap-img_canvasEle"
+          width={clientSize.width}
+          height={clientSize.height}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DrawCanvas;
