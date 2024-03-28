7ec267e0259b296a044c32d35c225352e962c13f
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2024-03-28 18:01:12 +0700

    feat(WebRTCComponent): build WebRTCComponent

--- src/components/WebRTCComponent/index.js
+++ src/components/WebRTCComponent/index.js
@@ -1,98 +1,78 @@
 import React, { useEffect, useRef } from "react";
 import "./style.scss";
-import io from "socket.io-client";
-
-import srcVideo from "./testVideo.mp4";
 
 const WebRTCComponent = () => {
-  const wssServer =
+  const webRTCsrc =
     "wss://hgdc-ios-api.dev.altasoftware.vn/go2rtc/api/ws?src=k/NJLE3RhVnvKvJ30+yq9R5I/DGMyee2Brn2/x+u1tjaOhTSBIC5XXV99tc=&media=video+audio";
   const videoRef = useRef(null);
-  const socketRef = useRef(null);
-  const peerConnectionRef = useRef(null);
 
   useEffect(() => {
-    socketRef.current = io.connect(wssServer);
-    peerConnectionRef.current = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-    });
-
-    socketRef.current.on("message", handleMessage);
-    setupLocalStream();
-
-    return () => {
-      socketRef.current.disconnect();
-      peerConnectionRef.current.close();
-    };
-  }, []);
-
-  const setupLocalStream = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true,
+    if (videoRef.current == null) return;
+    async function PeerConnection(media) {
+      const pc = new RTCPeerConnection({
+        iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
       });
-      stream
-        .getTracks()
-        .forEach((track) => peerConnectionRef.current.addTrack(track, stream));
 
-      if (videoRef.current != null) {
-        videoRef.current.srcObject = stream;
+      const localTracks = [];
+      if (/video|audio/.test(media)) {
+        const tracks = ["video", "audio"]
+          .filter((kind) => media.indexOf(kind) >= 0)
+          .map(
+            (kind) =>
+              pc.addTransceiver(kind, { direction: "recvonly" }).receiver.track,
+          );
+        localTracks.push(...tracks);
       }
-    } catch (e) {
-      console.debug("Error accessing media devices:", e);
-    }
-  };
+      if (videoRef.current != null) {
+        videoRef.current.srcObject = new MediaStream(localTracks);
+      }
 
-  const handleMessage = async (message) => {
-    const { type, offer, answer, candidate } = message;
-    switch (type) {
-      case "offer":
-        await handleOffer(offer);
-        break;
-      case "answer":
-        await handleAnswer(answer);
-        break;
-      case "candidate":
-        handleCandidate(candidate);
-        break;
-      default:
-        console.debug("Invalid message type:", type);
+      return pc;
     }
-  };
 
-  const handleOffer = async (offer) => {
-    try {
-      await peerConnectionRef.current.setRemoteDescription(offer);
-      const answer = await peerConnectionRef.current.createAnswer();
-      socketRef.current.emit("message", { type: "answer", answer });
-    } catch (e) {
-      console.debug("Error handling offer:", e);
-    }
-  };
+    async function connect(medi) {
+      console.debug("loading....");
+      const pc = await PeerConnection(media);
+      const ws = new WebSocket(webRTCsrc);
 
-  const handleAnswer = async (answer) => {
-    try {
-      await peerConnectionRef.current.setRemoteDescription(answer);
-    } catch (e) {
-      console.debug("Error handling answer:", e);
-    }
-  };
+      ws.addEventListener("open", () => {
+        pc.addEventListener("icecandidate", (ev) => {
+          if (!ev.candidate) return;
+          const msg = {
+            type: "webrtc/candidate",
+            value: ev.candidate.candidate,
+          };
+          ws.send(JSON.stringify(msg));
+        });
 
-  const handleCandidate = (candidate) => {
-    try {
-      peerConnectionRef.current.addIceCandidate(candidate);
-    } catch (error) {
-      console.debug("Error handling candidate:", error);
+        pc.createOffer()
+          .then((offer) => pc.setLocalDescription(offer))
+          .then(() => {
+            const msg = {
+              type: "webrtc/offer",
+              value: pc.localDescription?.sdp,
+            };
+            ws.send(JSON.stringify(msg));
+          });
+      });
+
+      ws.addEventListener("message", (ev) => {
+        const msg = JSON.parse(ev.data);
+        if (msg.type === "webrtc/candidate") {
+          pc.addIceCandidate({ candidate: msg.value, sdpMid: "0" });
+        } else if (msg.type === "webrtc/answer") {
+          pc.setRemoteDescription({ type: "answer", sdp: msg.value });
+        }
+      });
     }
-  };
+
+    const media = new URLSearchParams(webRTCsrc).get("media");
+    connect(media || "video+audio");
+  }, [videoRef.current]);
 
   return (
     <div className="web-rtc-compo">
       <video
-        // src={srcVideo}
-        // type="video/mp4"
-        // loop
         ref={videoRef}
         autoPlay={true}
         controls={true}
