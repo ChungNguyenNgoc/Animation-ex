6e31659dbca569e3bc89c176c83981dba299f2e3
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2024-04-04 10:36:02 +0700

    feat(CaptureScreenshot): build CaptureScreenshot

--- src/components/CaptureScreenshot/index.js
+++ src/components/CaptureScreenshot/index.js
@@ -1,11 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./style.scss";
 import QuocHocHue from "./quoc_hoc_hue.jpg";
 import html2canvas from "html2canvas";
 
 const CaptureScreenshot = () => {
   const divRef = useRef();
-  const [dataUrl, setDataUrl] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [imageFile, setImageFile] = useState(null);
 
   const captureScreenshot = () => {
     const divElement = divRef.current;
@@ -16,11 +17,36 @@ const CaptureScreenshot = () => {
 
     html2canvas(divElement).then((canvas) => {
       const base64Url = canvas.toDataURL("image/png");
-      setDataUrl(base64Url);
-      const newWindow = window.open();
-      newWindow.document.write('<img src="' + base64Url + '" />');
+      setImageUrl(base64Url);
+      // const newWindow = window.open();
+      // newWindow.document.write('<img src="' + base64Url + '" />');
     });
   };
+
+  const base64toFile = (base64String, filename) => {
+    const parts = base64String.split(";base64,");
+    const type = parts[0].split(":")[1];
+
+    const byteString = atob(parts[1]);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      uint8Array[i] = byteString.charCodeAt(i);
+    }
+
+    const blob = new Blob([uint8Array], { type });
+    return new File([blob], filename, { type });
+  };
+
+  useEffect(() => {
+    if (imageUrl == null) {
+      return;
+    }
+    const file = base64toFile(imageUrl, "capture_image");
+    if (file != null) {
+      setImageFile(file);
+    }
+  }, [imageUrl]);
 
   return (
     <div className="capture-screenshot">
