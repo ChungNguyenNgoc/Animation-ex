711b2a932f34f17d9be82eeb71eb9971e73c8fac
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2024-04-09 18:21:41 +0700

    feat(WebcamStream): build WebcamStream

--- src/components/WebcamChat/index.js
+++ src/components/WebcamChat/index.js
@@ -1,9 +1,13 @@
 import "./style.scss";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function WebcamStream() {
   const webcamRef = useRef(null);
   const peerRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+
+  console.debug("recordedChunks: ", recordedChunks);
 
   useEffect(() => {
     (async function initWebcam() {
@@ -28,13 +32,13 @@ function WebcamStream() {
         peerRef.current.addEventListener("icecandidate", (event) => {
           if (event.candidate) {
             // Send ICE candidate to the remote peer
-            console.debug("Sending ICE candidate:", event.candidate);
+            // console.debug("Sending ICE candidate:", event.candidate);
           }
         });
 
         // Listen for incoming stream from remote peer
         peerRef.current.addEventListener("track", (event) => {
-          console.debug("Remote stream received:", event.streams[0]);
+          // console.debug("Remote stream received:", event.streams[0]);
           // Use the remote stream to display video
         });
 
@@ -43,17 +47,41 @@ function WebcamStream() {
         await peerRef.current.setLocalDescription(offer);
 
         // Send the SDP offer to the remote peer
-        console.debug("Sending offer:", peerRef.current.localDescription);
+        // console.debug("Sending offer:", peerRef.current.localDescription);
       } catch (error) {
         console.error("Error accessing webcam:", error);
       }
     })();
   }, []);
 
+  const stopRecording = () => {
+    mediaRecorderRef.current.stop();
+  };
+
+  const handleDataAvailable = (e) => {
+    if (e.data.size > 0) {
+      mediaRecorderRef.current.stop();
+      setRecordedChunks((prev) => [...prev, e.data]);
+    }
+  };
+
+  const startRecording = () => {
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.srcObject);
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable,
+    );
+    mediaRecorderRef.current.start();
+  };
+
   return (
     <div className="webcam-chat">
       <div className="webcam-chat_wrap-video">
         <video ref={webcamRef} autoPlay playsInline width={960} height={540} />
+        <div>
+          <button onClick={startRecording}>Start Recording</button>
+          <button onClick={stopRecording}>Stop Recording</button>
+        </div>
       </div>
     </div>
   );
