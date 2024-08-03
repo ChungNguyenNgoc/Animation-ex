4acc6b8edc7422b49d35c80525e116f3fc15a9cf
ChungNguyenNgoc <baochungnguyenngoc@gmail.com>
2024-08-03 20:09:25 +0700

    update data

---
+++ src/components/EnglishSpeakingSuccess/ielts45.js
@@ -0,0 +1,58 @@
+export const optionsType = [
+  { label: "All", value: null },
+  { label: "Word", value: 1 },
+  { label: "Phrase", value: 2 },
+  { label: "Question", value: 3 },
+];
+
+export const optionsTopic = [{ label: "Relationships", value: 1 }];
+
+export const globalData = [
+  {
+    type: 1,
+    unit: 1,
+    data: [
+      "parents",
+      "father",
+      "mother",
+      "sibling",
+      "brother",
+      "sister",
+      "older sister",
+      "younger bother",
+      "grandparent",
+      "aunt",
+      "uncle",
+      "cousin",
+      "affectionate",
+      "supportive",
+      "trustworthy",
+      "loyal",
+      "respectful",
+      "bond",
+      "spend quality time",
+      "distant",
+      "loving",
+      "reliable",
+      "intimate",
+      "get along with",
+      "fall out with",
+    ],
+  },
+  {
+    type: 2,
+    unit: 1,
+    data: ["", "", "", "", "", "", ""],
+  },
+  {
+    type: 3,
+    unit: 1,
+    data: [
+      "Can you tell me your full name, please?",
+      "Where are you from?",
+      "Can you describe your family?",
+      "What do your parents do?",
+      "Do you spend a lot of time with your family?",
+    ],
+  },
+];
