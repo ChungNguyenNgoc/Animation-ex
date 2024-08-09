fc17a009b4e8b50615ee50839b37155e187c8a4c
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2024-08-09 21:10:56 +0700

    update data

--- src/components/EnglishSpeakingSuccess/ielts45.js
+++ src/components/EnglishSpeakingSuccess/ielts45.js
@@ -49,7 +49,7 @@ export const globalData = [
       "When I was younger, my cousins used to take us to the countryside on weekends, where we would explore nature and have picnics.",
       "It's important to communicate openly with your parents about your goals and dreams.",
       "My English teacher is very affectionate with her family, I appreciated her warmth and kindness.",
-      "Although uncle seem tough, but He is actually very affectionate with his teacher.",
+      "Although my uncle seems tough, but He is actually very affectionate with his teacher.",
       "I'm very trustworthy and never break promises.",
       "My close friend is very reliable and trustworthy, and everyone feels comfortable sharing their secrets with him.",
       "I highly recommend Jollie for the position, Being trustworthy is one of her best qualities, as people know they can rely on her in any situation.",
